477322b490029706ee5416590086c24bfecb639d
LinhLV <linhlv@Les-MacBook-Pro.local>
2025-12-30 21:08:32 +0700

    feat: prod config

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,8 @@ module RentalSearchApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Use SQL format for schema dump (required for PostGIS geography types)
+    config.active_record.schema_format = :sql
   end
 end
